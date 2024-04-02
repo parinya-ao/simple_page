8094446f5c3076053565a5125e1b840cf6000898
Parinya <parinyasa1029@gmail.com>
2024-04-02 13:59:19 +0700

    push all files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import React from 'react';
 import Login from './page/Login';
+import Register from './page/Register';
+import Admin from './page/Admin';
 
 function App() {
     return (
         <div>
             <Routes>
                 <Route path='login' element={<Login />} />
+                <Route path='admin' element={<Admin/>} />
             </Routes>
         </div>
     );
